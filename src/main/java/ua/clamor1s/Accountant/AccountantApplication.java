ed4758c4946c6b4fee7bdc154db360bb7bfcd343
yarikpo <yaroslavpopovich04@gmail.com>
2023-11-15 12:04:29 +0200

    initial commit

---
+++ src/main/java/ua/clamor1s/Accountant/AccountantApplication.java
@@ -0,0 +1,13 @@
+package ua.clamor1s.Accountant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/clamor1s/Accountant/AccountantApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.clamor1s.Accountant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
